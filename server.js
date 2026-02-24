933786c4e7dbc2e460e68443db2149e6f7bd1ccf
SoulVage <amirazarbahram1234@gmail.com>
2026-02-24 18:23:30 +0330

    last

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://soulvage.github.io/"],
+    origin: ["http://localhost:5173", "https://soulvage.github.io"],
   })
 );
 
